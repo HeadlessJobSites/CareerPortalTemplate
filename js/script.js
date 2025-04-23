15b98e224ae255bbf06074b824bf5496c411a7fc
Reboot Tech <tech@reboot.no>
2025-04-23 19:12:05 +0200

    chore: update html structure with BEM, change styles

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('.header');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 10) {
+      header.classList.add('header--scrolled');
+    } else {
+      header.classList.remove('header--scrolled');
+    }
+  });
+});
